f1faeeb2721168120d4fbacf98a7df0f92455f60
Lubov <lb.librab14@gmail.com>
2022-05-15 16:30:47 +0300

    added an app icon

--- Temperature-converter/ViewController.swift
+++ Temperature-converter/ViewController.swift
@@ -11,14 +11,15 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var celsiusLabel: UILabel!
     @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var resetButton: UIButton!
     @IBOutlet weak var slider: UISlider! {
         didSet {
             slider.maximumValue = 100
-            slider.minimumValue = 0
+            slider.minimumValue = -100
             slider.value = 0
             
-            slider.minimumTrackTintColor = .systemOrange
-            slider.maximumTrackTintColor = .systemCyan
+            slider.minimumTrackTintColor = .init(red: 248/255, green: 208/255, blue: 101/255, alpha: 1)
+            slider.maximumTrackTintColor = .init(red: 150/255, green: 197/255, blue: 250/255, alpha: 1)
         }
     }
         
@@ -28,6 +29,18 @@ class ViewController: UIViewController {
         
         let temperatureFahrenheit = Int(round((sender.value * 9 / 5) + 32))
         fahrenheitLabel.text = "\(temperatureFahrenheit)ºF"
+    }
+    
+    @IBAction func resetSlider(_ sender: UIButton) {
+        slider.value = 0
+        celsiusLabel.text = "0ºC"
+        fahrenheitLabel.text = "0ºF"
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        resetButton.layer.cornerRadius = 30
     }
 }
 
